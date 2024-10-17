88e20caca29f1135b371413f8e4f69a598e28e8d
B12_309_21\b12s309 <julioochoamedrano@gmail.com>
2024-10-17 11:38:31 -0500

    metodo init Regla validacion

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,6 @@
 # TODO: Implementa el código del ejercicio aquí
+from abc import ABC,abstractmethod,
+class ReglaValidacion(ABC):
+
+    def __init__(self, longitud_esperada:int):
+        self._longitud_esperada: int = longitud_esperada
