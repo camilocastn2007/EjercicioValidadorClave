2326ab5aa9a227f19e274663cbf679efe6a492bb
B12_309_21\b12s309 <julioochoamedrano@gmail.com>
2024-10-17 11:39:17 -0500

    metodos de verificacion de clave esperada

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -4,3 +4,17 @@ class ReglaValidacion(ABC):
 
     def __init__(self, longitud_esperada:int):
         self._longitud_esperada: int = longitud_esperada
+
+    def _validar_longitud(self, clave):
+        return len(clave) > self._longitud_esperada
+
+    def _contiene_mayuscula(self, clave):
+        return any(a.isupper() for a in clave)
+
+    def _contiene_minuscula(self, clave):
+        return any(a.islower() for a in clave)
+
+
+    def _contiene_numero(self,clave):
+        return any(a.isdigit() for a in clave)
+
