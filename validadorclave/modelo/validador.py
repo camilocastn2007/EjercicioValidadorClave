ee54c6ac6d6cce67ddbf815f4f0606f9188ca4ab
B12_309_21\b12s309 <julioochoamedrano@gmail.com>
2024-10-17 11:41:28 -0500

    Clase ValidacionGanimedes

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -22,3 +22,29 @@ class ReglaValidacion(ABC):
     def es_valida(self, clave):
         pass
 
+class ReglaValidacionGanimedes(ReglaValidacion):
+    CARACTERES_ESPECIALES = '@_#$%'
+
+    def __init__(self):
+        super().__init__(8)  # La longitud esperada es mayor a 8
+
+    def _contiene_caracter_especial(self, clave):
+        return any(c in self.CARACTERES_ESPECIALES for c in clave)
+
+    def es_valida(self, clave):
+        if not self._validar_longitud(clave):
+            raise Exception("La clave debe tener una longitud de más de 8 caracteres")
+        if not self._contiene_mayuscula(clave):
+            raise Exception("La clave debe contener al menos una letra mayúscula")
+        if not self._contiene_minuscula(clave):
+            raise Exception("La clave debe contener al menos una letra minúscula")
+        if not self._contiene_numero(clave):
+            raise Exception("La clave debe contener al menos un número")
+        if not self._contiene_caracter_especial(clave):
+            raise Exception("La clave debe contener al menos un caracter especial (@, _, #, $, %)")
+        return True
+
+def _contiene_calisto(self, clave):
+    calisto = 'calisto'
+    mayusculas = sum(1 for c in clave if c.isupper())
+    return 'calisto' in clave.lower() and 2 <= mayusculas < 6
